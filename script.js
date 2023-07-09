9ee825da8e36f55707420bc82949f39764011de6
Pixls112 <nguyen.phillip112@gmai.com>
2023-07-09 16:59:27 -0700

    Added to Index.html and Style.css

---
+++ script.js
@@ -0,0 +1,29 @@
+var startButn = document.querySelector("#startBtn");
+
+var questions = [
+    {
+        title: "Javascript is an _______ language?",
+        choices: ["Object-Orientated", "Object-Based", "Procedural", "None of the above"],
+        answer: "Object-Orientated"
+    },
+    {
+        title: "Which of the following keywords is used to define a variable in Javascript?",
+        choices: ["var", "let", "A and B", "None of the above"],
+        answer: "A and B"
+    },
+    {
+        title: "Which of the following methods is used to access HTML elements using Javascript?",
+        choices: ["getElementbyid()", "getElementsByClassName()", "A and B", "None of the above"],
+        answer: "A and B"
+    },
+    {
+        title: "Upon encountering empty statements, what does the Javascript Interpreter do?",
+        choices: ["Throws an error", "Ignores the statement", "Gives a warning", "None of the above"],
+        answer: "Ignores the statement"
+    },
+    {
+        title: "Arrays in JavaScript are defined by which of the following statements?",
+        choices: ["Values", "Objects", "String", "Functions"],
+        answer: "Values"
+    },
+]
