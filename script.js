f32cfff97a05d8e8cc777bcf0ad5aeb0f90e5b74
Pixls112 <nguyen.phillip112@gmai.com>
2023-07-10 22:33:31 -0700

    updated javascript and redid questiions var

--- script.js
+++ script.js
@@ -1,33 +1,20 @@
 var starterBtn = document.querySelector("#startBtn");
 var starterPage = document.querySelector("#starter-page");
-var questionPage = document.querySelector("#question-page")
+var questionPage = document.querySelector("#question-page");
+var sampleQuestions = document.querySelector(".question");
+var answerBtn = document.querySelector("#answerButtons");
+var currentIndex = 0;
 
 var questions = [
     {
-        title: "Javascript is an _______ language?",
-        choices: ["Object-Orientated", "Object-Based", "Procedural", "None of the above"],
-        answer: "Object-Orientated"
-    },
-    {
-        title: "Which of the following keywords is used to define a variable in Javascript?",
-        choices: ["var", "let", "A and B", "None of the above"],
-        answer: "A and B"
-    },
-    {
-        title: "Which of the following methods is used to access HTML elements using Javascript?",
-        choices: ["getElementbyid()", "getElementsByClassName()", "A and B", "None of the above"],
-        answer: "A and B"
-    },
-    {
-        title: "Upon encountering empty statements, what does the Javascript Interpreter do?",
-        choices: ["Throws an error", "Ignores the statement", "Gives a warning", "None of the above"],
-        answer: "Ignores the statement"
-    },
-    {
-        title: "Arrays in JavaScript are defined by which of the following statements?",
-        choices: ["Values", "Objects", "String", "Functions"],
-        answer: "Values"
-    },
+        question: "Javascript is an _______ language?",
+        answers: [
+            {text: "Object-Orientated", correct:true},
+            {text: "Object-Based", correct:false},
+            {text: "Procedural", correct:false},
+            {text: "None of the above", correct:false},
+        ]
+    }
 ]
 
 
@@ -36,9 +23,23 @@ starterBtn.addEventListener("click", startGame)
 function startGame() {
     starterPage.classList.add("hidePage")
     questionPage.classList.remove("hidePage")
+    nextQuestions()
     console.log('Start')
-
 }
 
-  
 
+function nextQuestions() {
+    sampleQuestions.innerText = questions[currentIndex].question
+    var question = questions[currentIndex]
+    question.answers.forEach(answer => {
+        var button = document.createElement("button")
+        button.innerText = answer.text
+        button.classList.add('btn')
+
+    button.addEventListener("click", function(){
+
+    }) 
+        answerBtn.appendChild(button)
+    })
+ 
+}
