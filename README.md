45d951d476dbfe71b95f9552817030d8be8978a2
Pixls112 <nguyen.phillip112@gmai.com>
2023-07-12 22:16:20 -0700

    added comments to .js and updated readme

---
+++ README.md
@@ -0,0 +1,40 @@
+# Phil's Timed Quiz
+
+## Description
+
+This weeks challenge, I was tasked with creating a quiz application that would have a countdown timer, multiple questions, and would log the user's score/initials. I would use all the previous things I have learned and also additionally incorporate javascript into this application.
+## Installation
+
+To install my code you can use clone the repo using either the HTML or ssh key to add to your local machine.
+
+## Usage
+
+The main usage of this project was to show my ability to create something from the ground up given only a small amount of details to work off of and to try and showcase my ability and understanding of javascript. From what I have learned, Javascript is useful as it allows me cut down on the amount of text I would have placed onto my HTML and or CSS to mimmick the same action as it would have in javascript. 
+
+## Screenshot
+
+The following image shows the web application's appearance and functionality:
+
+
+## Credit
+
+I was able to accomplish my goal with the help of this video and website as a reference when working on my project.
+https://www.youtube.com/watch?v=riDzcEQbX6k
+
+I would also like to thank Justin Feagin a classmate of mine who also helped guide me in the right direction when I was lost.
+
+## Access Site
+
+This is the link to access the site launched by github
+
+## Github
+
+https://github.com/Pixls112/phils-timed-quiz
+
+## Launched Website
+
+
+
+## License
+
+Please refer to the LICENSE in the repo.
--- script.js
+++ script.js
@@ -1,23 +1,23 @@
+//These are my global defined var
 var starterBtn = document.querySelector("#startBtn");
 var starterPage = document.querySelector("#starter-page");
 var questionPage = document.querySelector("#question-page");
 var sampleQuestions = document.querySelector(".question");
 var answerBtn = document.querySelector("#answerButtons");
 var currentIndex = 0;
-var timerCount = document.querySelector("#timer-gauge")
+var timerCount = document.querySelector("#timer-gauge");
 var countDown = 60;
-var endingPage = document.querySelector("#end-page")
-var userScore = document.querySelector("#score")
+var endingPage = document.querySelector("#end-page");
+var userScore = document.querySelector("#score");
 var score = 0;
-var scoreBtn = document.querySelector("#highscoreBtn")
-var scorePage = document.querySelector(".highscore")
-var initialS = []
-var finalScore = []
-var recentScore = document.querySelector(".recentScores")
-var refreshBtn = document.querySelector("#refresh-button")
-var recentScore2 = document.querySelector(".recentScores2")
-
-
+var scoreBtn = document.querySelector("#highscoreBtn");
+var scorePage = document.querySelector(".highscore");
+var initialS = [];
+var finalScore = [];
+var recentScore = document.querySelector(".recentScores");
+var refreshBtn = document.querySelector("#refresh-button");
+var recentScore2 = document.querySelector(".recentScores2");
+//This variable is where all the questions are stored and can be accessed as an array when called upon through other functions
 var questions = [
     {
         question: "Javascript is an _______ language?",
@@ -44,33 +44,37 @@ var questions = [
         answers: [{text:"Values"}, {text:"Objects"}, {text:"String"}, {text:"Functions"}],
         correct: "Values"
     }   
-]
+];
 
-refreshBtn.addEventListener("click", refreshPage)
+//Makes it so that when the button is clicked on the page it loads the function set
+refreshBtn.addEventListener("click", refreshPage);
 
-scoreBtn.addEventListener("click", scorepage)
+scoreBtn.addEventListener("click", scorepage);
 
-starterBtn.addEventListener("click", startGame)
+starterBtn.addEventListener("click", startGame);
 
+//This function is used to start the game. When the startGame function is called it hides the other html elements around the page and shows the correct one to display for the user
+//This will also ask for the players initials at the beginning of the quiz to record there score
 function startGame() {
     starterPage.classList.add("hidePage")
     questionPage.classList.remove("hidePage")
     initialS = prompt("Enter your initials")
-    
     console.log(initialS)
     nextQuestions()
     setTime()
     userScore.innerText = "Score: " + score;
-}
-
+};
+//Similar to the startGame function this one simply hides the other html elements and reveals the score section in the html
 function scorepage() {
     starterPage.classList.add("hidePage")
     scorePage.classList.remove("hidePage")
     endingPage.classList.add("hidePage")
     recentScore.innerText = localStorage.getItem("final-score")
 
-}
+};
 
+//This function is used to populate the html page with new buttons with the answers given in a variable onto them
+//When the correct answer is clicked the function will take us to the next question where the process repeats itself until we run out of questions to call on
 function nextQuestions() {
     sampleQuestions.innerText = questions[currentIndex].question
     var question = questions[currentIndex]
@@ -92,7 +96,7 @@ function nextQuestions() {
         })
     })
  
-}
+};
 
 function proceedQuestion() {
     resetState()
@@ -115,7 +119,7 @@ function proceedQuestion() {
         }
     })
 })
-}
+};
 
 function proceedQuestion2() {
     resetState()
@@ -138,12 +142,12 @@ function proceedQuestion2() {
         }
     })
 })
-}
+};
 
 function proceedQuestion3() {
-    resetState()
+    resetState();
     currentIndex++;
-    sampleQuestions.innerText = questions[currentIndex].question 
+    sampleQuestions.innerText = questions[currentIndex].question
     var question = questions[currentIndex]
     question.answers.forEach(answer => {
     var button = document.createElement("button")
@@ -161,7 +165,7 @@ function proceedQuestion3() {
         }
     })
 })
-}
+};
 
 function proceedQuestion4() {
     resetState()
@@ -184,36 +188,39 @@ function proceedQuestion4() {
         }
     })
 })
-}
+};
 
-
+//This function is called on after the final correct answer is clicked. It loads  up the endPage function while showing the player there initials and score for the quiz
 function proceedQuestionFinal() {
     finalScore = initialS + " : " + score
     console.log(finalScore)
     localStorage.setItem("final-score", finalScore)
     endPage()
-}
+};
 
-
+//This function is similar to that of startGame where it hides other elements and shows the player the hidden html section appropriate for the situation
 function endPage() {
     endingPage.classList.remove("hidePage")
     questionPage.classList.add("hidePage")
     recentScore2.innerText = localStorage.getItem("final-score")
     timeReset()
-}
+};
 
+//This function resets the html to its original point so that each time a question is answered the old questions are removed and are able to be repopulared with the new answers.
 function resetState() {
     while(answerBtn.firstChild) {
         answerBtn.removeChild(
             answerBtn.firstChild
         )
     }
-}
+};
 
+//This function simply refreshes the page
 function refreshPage() {
     window.location.reload()
-}
+};
 
+// This function adds a countdown timer for the player which when the timer hits 0 the endPage function from earlier is called upon
 function setTime() {
     
     var timerInterval = setInterval(function(){
@@ -225,8 +232,9 @@ function setTime() {
             endPage()
         }
     },1000)
-}
+};
 
+//This simply sets the countdown timer to 0 when the player finishes the quiz
 function timeReset(){
     countDown=1
-}
+};
